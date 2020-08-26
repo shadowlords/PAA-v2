59c188d7e450b5ef9f21a34eaa91fc0b25f481c9
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-08-26 18:40:35 +0200

    Update autogunspawn.lua

--- autogunspawn.lua
+++ autogunspawn.lua
@@ -1,9 +1,7 @@
-if game.Players.LocalPlayer.UserId == "155615604" then
+if game.Players.LocalPlayer.UserId == "1191567359" then
     local slot1 = "M9"
     local slot2 = "M4A1"
     local slot3 = "Remington 870"
-elseif game.Players.LocalPlayer.UserId == "7" then
-    --repeat
 end
 workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver[slot1].ITEMPICKUP)
 workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver[slot2].ITEMPICKUP)
